dc403ccf584dbbb54ff996da43ba49db4152a50c
pinky-banik <pinky.rani.banik@gmail.com>
2022-11-10 23:34:26 +0600

    ok

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput } from 'react-native'
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import login from '../../assets/login.jpg';
@@ -6,8 +6,14 @@ import login from '../../assets/login.jpg';
 export default function Login() {
   return (
     <SafeAreaView className="bg-white flex-1 items-center">
-      <Image source={login} style={{ width: 400, height: 300, backgroundColor: "red" }}/>
-      <Text className="text-pink-500">Hi</Text>
+      <Image source={login} style={{ width: 400, height: 300 }}/>
+      <Text className="text-pink-500 font-bold text-lg text-center">Never forget your notes</Text>
+
+      <View>
+        <TextInput placeholder='Email' className="h-12 border w-96 text-center "/>
+        <TextInput placeholder='Password' className="h-12 border w-96 text-center my-2"/>
+        <TextInput placeholder='Confirm Password' className="h-12 border w-96 text-center "/>
+      </View>
     </SafeAreaView>
   )
 }
